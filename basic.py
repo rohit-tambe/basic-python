4ed968681d41e0783ebdc8c2022181918f645f5c
rohit-tambe <rohit.tambe@billcloud.in>
2020-07-02 15:36:08 +0530

    Initiate

---
+++ basic.py
@@ -0,0 +1,37 @@
+# task 1
+# add two numbers and print output
+# step 1 take input 1
+number1 = 9
+# print("a = ", a)
+
+# step 2 take input 2
+number2 = 8.10
+# print("b = ", b)
+
+# step 3 add input and store in varible
+output = number1+number2
+
+# step 4 print output
+print("Output ", output)
+
+print((3*5)+10)
+
+print((50 - 5*6) / 4)
+
+print(17 // 3)
+# 5.666666666666667
+
+print("15 % 2  = ", 15 % 2)
+
+print("2 ** 3 = ", 2 ** 3)
+
+print("pow(2, 3) = ", pow(2, 3))
+
+d = 3.9005
+print("d = ", d)
+
+print("round(d) = ", round(d))
+name = "Rohit"
+s = 'Sonu'
+
+print(name, s)
---
+++ strings-basic.py
@@ -0,0 +1,37 @@
+print("don't")
+print("rohit\ttambe")
+
+# tab
+print(3 * 'sonu\t')
+
+# new line
+print(3 * 'sonu\n')
+
+# backspace
+print(3 * 'sonu\b')
+
+# concatenated
+print("Rohit " + "Sonu")
+
+address = ('Karmayog hsg b3 304 \n'
+           ' Pune')
+# 1
+print(address)
+
+print('sonu' * 3)
+
+sonuName = "Aditya"
+rohitName = "Rohit"
+junkChar = "zfsdfgsdfsdfsdfsdfsdsdfdsgfdgdfgsdgsdfssd"
+print(sonuName, len(sonuName), sonuName[5])  # index Aditya len()
+print(rohitName, len(rohitName), rohitName[4])
+
+print(sonuName[0])
+print(rohitName[0])
+
+index = len(sonuName)-2
+
+print(sonuName[index])
+
+
+print()
